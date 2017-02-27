0aca32ecf4017c6cd7bb1e0e3980ed54ec256399
add2point71dots <alcland@gmail.com>
2017-02-27 10:56:55 -0800

    adds & tests initialize, order_item, and add_tax

--- lib/restaurant_bill.rb
+++ lib/restaurant_bill.rb
@@ -1,12 +1,21 @@
-# class RestaurantBill
-#   attr_reader :ordered_items
+class RestaurantBill
+  attr_reader :ordered_items
 
-#   def initialize
-#     @ordered_items = []
-#   end
+  def initialize
+    @ordered_items = []
+  end
 
-#   def order_item(item, cost)
-#     @ordered_items << [item, cost]
-#   end
+  def order_item(item, cost)
+    @ordered_items << [item, cost]
+  end
 
-# end
+  def add_tax
+    total = 0
+
+    @ordered_items.each do |item|
+      total += item[1]
+    end
+
+    total *= 1.096
+  end
+end
--- specs/restaurant_bill_spec.rb
+++ specs/restaurant_bill_spec.rb
@@ -2,3 +2,40 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
 require_relative '../lib/restaurant_bill'
+
+Minitest::Reporters.use!
+Minitest::Reporters::SpecReporter.new
+
+describe "RestaurantBill" do
+  before do
+    @bill = RestaurantBill.new
+  end
+
+  it "A bill is created with an empty ordered_items" do
+    @bill.ordered_items.must_be_instance_of Array
+    @bill.ordered_items.length.must_equal 0
+  end
+
+  it "Method order_item includes new item in ordered_items" do
+    @bill.order_item("apple", 1.0)
+    @bill.ordered_items.must_include ["apple", 1.0]
+  end
+
+  it "Method order_item includes items when called multiple times" do
+    @bill.order_item("apple", 1.0)
+    @bill.order_item("soup", 7.5)
+    @bill.order_item("tea", 2.0)
+
+    @bill.ordered_items.must_include ["apple", 1.0]
+    @bill.ordered_items.must_include ["apple", 1.0]
+    @bill.ordered_items.must_include ["apple", 1.0]
+  end
+
+  it "add_tax returns total plus tax" do
+    @bill.order_item("apple", 1.0)
+    @bill.order_item("soup", 7.5)
+    @bill.order_item("tea", 2.0)
+
+    @bill.add_tax.must_equal (10.5 * 1.096)
+  end
+end
